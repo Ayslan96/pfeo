10b87579376235aaac828415071e70848d37fb1b
Ayslan Câmara <ayslan.trabalho@gmail.com>
2019-05-28 20:44:36 -0300

    adiçao menu.h e .cpp

---
+++ menu.cpp
@@ -0,0 +1,44 @@
+char op;
+
+	cout<<"1. Cadastrar novo animal\n";
+	cout<<"2. Remover animal\n";
+	cout<<"3. Alteração de cadastro de animal\n";
+	cout<<"4. Consultar animal\n";
+	cout<<"5. Cadastrar novo funcionario\n";
+	cout<<"6. Remover funcionario\n";
+	cout<<"7. Alteração de cadastro de funcionario\n";
+	cout<<"8. Consultar funcionario\n\n";
+	cout<<"ENTRE COM A OPCAO DESEJADA: ";
+	cin>>op;
+
+
+	switch(op){
+
+	case '1':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '2':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '3':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '4':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '5':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '6':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '7':
+		cout<<"estamos na opcao "<<op;
+		break;
+	case '8':
+		cout<<"estamos na opcao "<<op;
+		break;
+	default:cout<<"Opcao "<<op<<" invalida. ";
+		break;
+
+		}
