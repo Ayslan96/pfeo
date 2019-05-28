b01bc6dfad6a699d185667dbbca17a4c02eae8e4
Luan Rocha <38924909+luanrocha98@users.noreply.github.com>
2019-05-28 14:11:12 -0300

    leitura do arquivo e classe animal

---
+++ infoanimal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class InfoAnimal {
+
+	int numero;
+	std::string classe;
+	std::string nome;
+	std::string nome_Cien;
+	char sexo;
+	float tamanho_Metros;
+	std::string dieta;
+	int veterinario;
+	int tratador;
+	std::string nome_Batismo;
+
+public:
+
+	
+}
