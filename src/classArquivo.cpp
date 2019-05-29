2f554d18b21bcf9d1647cc3fcf6dafc55350964c
andrehabib <42250406+andrehabib@users.noreply.github.com>
2019-05-29 15:53:06 -0300

    Add files via upload

---
+++ src/classArquivo.cpp
@@ -0,0 +1,8 @@
+#include "infoanimal.h"
+
+void Arquivo_Geral::leituraArquivo_Animal(ifstream arquivo_){
+}
+
+void Arquivo_Geral::leituraArquivo_Funcionario(ifstream arquivo2_){
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "interfaces.h"
+#include "menu.h"
+
+
+int main(){
+	
+	menu main;
+
+	main.menuM();
+
+	
+
+	return 0;
+}
