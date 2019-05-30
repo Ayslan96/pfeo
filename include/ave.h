4407caec1b92d75dc850a64b176907bd70f22fab
André Habib Fonseca Montenegro <andrehabib@A204-13L.imd.local>
2019-05-30 12:51:05 -0300

    teste

--- include/ave.h
+++ include/ave.h
@@ -10,7 +10,7 @@ private:
 		
 public:
 	//Ave();
-	Ave(int id, std::string classe, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, std::sstring nome_batismo, double tamanho_do_bico_cm, double envergadura_das_asas);
+	Ave(double tamanho_do_bico_cm, double envergadura_das_asas) : Animal(int id, std::string classe, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, std::string nome_batismo);
 	~Ave();
 	friend std::ostream& operator << (std::ostream &saida, const Animal& p){
     saida << "Id: " << p.m_id << " Classe: " << p.m_class<< " Nome cientifico: " << p.m_nome_cientifico << " Sexo: " << p.m_nome_cientifico << " Tamanho: " << p.m_tamanho << " Dieta: " << p.m_dieta << " Nome de batismo: " << p.m_nome_batismo << "Tamanho de bico: " << p.m_tamanho_do_bico_cm << " Envergadura das asas: " << p.m_envergadura_das_asas;
