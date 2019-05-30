f0b4dc1407e53c80cf7277a5ac87a8f8cef19cd0
andrehabib <andre_hfm@hotmail.com>
2019-05-30 20:16:42 -0300

    Sobrecarga de operadores

--- include/animal.h
+++ include/animal.h
@@ -6,7 +6,7 @@
 //#include "tratador.h"
 
 class Animal {
-	
+	 
 protected:
 	int m_id;
 	std::string m_class;
@@ -23,9 +23,11 @@ public:
 	Animal(void);
 	Animal(int m_id, std::string m_class, std::string m_nome_cientifico, char m_sexo, double m_tamanho, std::string m_dieta, std::string m_nome_batismo);
     ~Animal();
-    virtual void print() = 0;
- 
+  	virtual std::ostream& print(std::ostream&) const = 0;
+    friend std::ostream& operator << (std::ostream& os, const Animal& b) {
+		return b.print(os);
+	}
 
-};
+ };
 
 #endif
--- include/ave.h
+++ include/ave.h
@@ -7,12 +7,18 @@ class Ave : public Animal{
 private:
 	double m_tamanho_do_bico_cm;
 	double m_envergadura_das_asas;
+	
 		
 public:
 	Ave(void);
-	Ave(int id, std::string classe, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, std::string nome_batismo, double m_tamanho_do_bico_cm, double m_envergadura_das_asas);
-	~Ave();
-	void print();
+	Ave(int m_id, std::string m_class, std::string m_nome_cientifico, char m_sexo, double m_tamanho, std::string m_dieta, std::string m_nome_batismo, double m_tamanho_do_bico_cm, double m_envergadura_das_asas);
+	~Ave();	
+	std::ostream& print(std::ostream os) const{
+    	return os << "Id: " << m_id << " Classe: " << m_class<< " Nome cientifico: " << m_nome_cientifico << " Sexo: " << m_nome_cientifico << " Tamanho: "	<< m_tamanho << " Dieta: " << m_dieta << " Nome de batismo: " << m_nome_batismo << " Tamnho do bico: " << m_tamanho_do_bico_cm << " Envergadura das asas: " << m_envergadura_das_asas;
+	}
 };
  
 #endif
+
+
+//"Id: " << p.m_id << " Classe: " << p.m_class << " Nome cientifico: " << p.m_nome_cientifico << " Sexo: " << p.m_sexo << " Tamanho: " << p.m_tamanho << " Dieta: " << p.m_dieta << " Nome de batismo: " << p.m_nome_batismo <<
