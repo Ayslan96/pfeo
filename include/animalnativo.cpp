7778511926d883e597c4cac2a61b80d68cd42283
andrehabib <andre_hfm@hotmail.com>
2019-06-25 15:46:15 -0300

    Consertei os erros na classe arquivo

--- include/animalnativo.cpp
+++
@@ -1,7 +0,0 @@
-#include "animalnativo.h"
-
-AnimalNativo::AnimalNativo(void){}
-AnimalNativo::AnimalNativo(std::string m_autorizacao_ibama, std::string m_uf_origem, std::string m_autorizacao):AnimalSilvestre(m_autorizacao_ibama),
-m_uf_origem (m_uf_origem),m_autorizacao(m_autorizacao) {}
-
-AnimalNativo::~AnimalNativo(){}
--- include/animalsilvestre.cpp
+++
@@ -1,7 +0,0 @@
-#include "animalsilvestre.h"
-
-AnimalSilvestre::AnimalSilvestre(void){}
-
-AnimalSilvestre::AnimalSilvestre(std::string m_autorizacao_ibama):m_autorizacao_ibama(m_autorizacao_ibama){}
-
-AnimalSilvestre::~AnimalSilvestre(){}
