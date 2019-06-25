cb2c422a069c27df1699ce99bc20b47fa4e2f991
Ayslan Camara de Moura Silveira <ayslan@A307-25L.imd.local>
2019-06-25 15:32:54 -0300

    arrumando teste entrada

--- teste/classArquivo.cpp
+++ teste/classArquivo.cpp
@@ -42,3 +42,31 @@ if (!arquivoDeEntrada)
 	arquivoDeEntrada.close();/*!<Fecha o arquivo.txt*/
 }
 
+switch principal[0]:
+
+case 
+AnfibioNativo::AnfibioNativo(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, string m_nome_batismo,
+		int m_total_de_mudas, Date m_ultima_muda, string m_uf_origem, string m_autorizacao) 12
+
+
+AnfibioExotico::AnfibioExotico(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, string m_nome_batismo,
+		int m_total_de_mudas, Date m_ultima_muda, string m_pais_origem): 11
+	
+AveNativo::AveNativo(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, 
+	string m_nome_batismo, double m_tamanho_do_bico_cm, double m_envergadura_das_asas, string m_pais_origem ):11
+
+AveExotico::AveExotico(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, 
+	string m_nome_batismo, double m_tamanho_do_bico_cm, double m_envergadura_das_asas, string m_pais_origem ):10
+	
+	
+MamiferoNativo::MamiferoNativo(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
+		std::string m_cor_pelo, std::string m_autorizacao_ibama, std::string m_uf_origem, std::string m_autorizacao):11
+		
+MamiferoExotico::MamiferoExotico(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
+		std::string m_cor_pelo, std::string m_autorizacao_ibama, std::string m_pais_origem):10
+		
+ReptilNativo::ReptilNativo(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
+		bool m_venenoso, std::string m_tipo_de_veneno, std::string m_autorizacao_ibama, std::string m_uf_origem, std::string m_autorizacao):12
+
+ReptilExotico::ReptilExotico(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
+		bool m_venenoso, std::string m_tipo_de_veneno, std::string m_autorizacao_ibama, std::string m_pais_origem):11
