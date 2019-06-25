393ac4855795a13b9dbc966d86778c7fe1fc43b0
Ayslan Câmara <ayslan.trabalho@gmail.com>
2019-06-24 21:59:25 -0300

    trecho exemplo para ler e instanciar

--- src/classArquivo.cpp
+++ src/classArquivo.cpp
@@ -1,5 +1,13 @@
 #include "../include/classArquivo.h"
-
+/*
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <vector>
+*/
 void Arquivo_Geral::leituraArquivo_Animal(){
 	arquivoAnimal.open("animais.txt");
 	if(arquivoAnimal.is_open()){
@@ -59,3 +67,34 @@ void Arquivo_Geral::fecharArquivo_Animal(){
 	arquivoAnimal.close();
 } 
 
+
+//void memoria::preencheMemoria(){
+
+
+//	ifstream arquivoDeEntrada;/*!<Instancia um objeto da classe  'ifstream'*/
+//	string S, aux; /*!<Variáveis auxiliares*/
+
+//	arquivoDeEntrada.open ("data/arquivo.txt", ios::in); /*!<O Objeto arquivoDeEntrada usa o arquivo arquivo.txt como entrada*/
+
+//	if (!arquivoDeEntrada)
+//	{	
+		/*!<Caso haja algum problema na abertura de arquivo.txt a mensagem é exibida*/
+//		cout << "Problemas na abertura do arquivo" << endl;
+//		exit(1);
+//	}
+
+//	while(!arquivoDeEntrada.eof()){
+		/*!<Enquanto o arquivo não chegar ao final as instruções do laço serão executadas*/
+//		getline(arquivoDeEntrada, S); /*!<Insere o conteúdo do objeto arquivoDeEntrada na string S*/
+
+//		std::istringstream iss(S); /*!<Instancia iss com S como entrada*/
+//		std::string token; /*!<Cria string token*/
+
+//		while (std::getline(iss, token, ',')) 
+//		{
+			/*!<Enquanto houverem separadores ',' as instruções do laço serão executadas*/
+//			principal.push_back(stoi(token)); /*!<Pega o numero no formato de string, converte e insere na memória*/
+//		}
+	//}
+///	arquivoDeEntrada.close();/*!<Fecha o arquivo.txt*/
+//}
