82ebe1af1c43c92ea0b656b6bf0c2452df26399a
Ayslan Camara de Moura Silveira <ayslan@A307-25L.imd.local>
2019-06-25 16:15:35 -0300

    pasta teste

--- teste/classArquivo.cpp
+++ teste/classArquivo.cpp
@@ -14,7 +14,7 @@ vector<string> principal;
 ifstream arquivoDeEntrada;/*!<Instancia um objeto da classe  'ifstream'*/
 string S, aux; /*!<Variáveis auxiliares*/
 
-arquivoDeEntrada.open ("funcionarios.txt", ios::in); /*!<O Objeto arquivoDeEntrada usa o arquivo arquivo.txt como entrada*/
+arquivoDeEntrada.open ("animais.txt", ios::in); /*!<O Objeto arquivoDeEntrada usa o arquivo arquivo.txt como entrada*/
 
 if (!arquivoDeEntrada)
 {	
@@ -30,10 +30,10 @@ if (!arquivoDeEntrada)
 		std::istringstream iss(S); /*!<Instancia iss com S como entrada*/
 		std::string token; /*!<Cria string token*/
 		while (std::getline(iss, token, ';')) 
-		{	
-			if (!getline)
+		{
 			/*!<Enquanto houverem separadores ',' as instruções do laço serão executadas*/
 			principal.push_back(token); /*!<Pega o numero no formato de string, converte e insere na memória*/
+			
 		}
 }
 
@@ -44,45 +44,45 @@ if (!arquivoDeEntrada)
 }
 
 
-switch principal[0]:
+//switch principal[0]:
 
-case "11":
+//case "11":
 
 /*
 AveNativo::AveNativo(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, 
 	string m_nome_batismo, double m_tamanho_do_bico_cm, double m_envergadura_das_asas, string m_pais_origem ):11
 */
-case "12":
+//case "12":
 /*AveExotico::AveExotico(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, 
 	string m_nome_batismo, double m_tamanho_do_bico_cm, double m_envergadura_das_asas, string m_pais_origem ):10
 */	
 
-case "21": 
+//case "21": 
 
 /*AnfibioNativo::AnfibioNativo(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, string m_nome_batismo,
 		int m_total_de_mudas, Date m_ultima_muda, string m_uf_origem, string m_autorizacao) 12
 */
-case "22":
+//case "22":
 /*
 AnfibioExotico::AnfibioExotico(int m_id, string m_classe, string m_nome_cientifico, char m_sexo, int m_tamanho, string m_dieta, string m_nome_batismo,
 		int m_total_de_mudas, Date m_ultima_muda, string m_pais_origem): 11
 */
 
-case "31":
+//case "31":
 /*MamiferoNativo::MamiferoNativo(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
 		std::string m_cor_pelo, std::string m_autorizacao_ibama, std::string m_uf_origem, std::string m_autorizacao):11
 */		
 
-case "32":
+//case "32":
 /*MamiferoExotico::MamiferoExotico(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
 		std::string m_cor_pelo, std::string m_autorizacao_ibama, std::string m_pais_origem):10
 */
 
-case "41":
+//case "41":
 /*ReptilNativo::ReptilNativo(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
 		bool m_venenoso, std::string m_tipo_de_veneno, std::string m_autorizacao_ibama, std::string m_uf_origem, std::string m_autorizacao):12
 */
-case "42":
+//case "42":
 /*ReptilExotico::ReptilExotico(int m_id, std::string m_classe, std::string m_nome_cientifico, char m_sexo, int m_tamanho, std::string m_dieta, std::string m_nome_batismo,
 		bool m_venenoso, std::string m_tipo_de_veneno, std::string m_autorizacao_ibama, std::string m_pais_origem):11
 */
