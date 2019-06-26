97680bb2ac4ba1809f626107f30678f3b0e58cd5
Ayslan96 <ayslan.trabalho@gmail.com>
2019-06-26 12:00:03 -0300

    falta domestcos

--- teste/classArquivo.cpp
+++ teste/classArquivo.cpp
@@ -39,56 +39,99 @@ if (!arquivoDeEntrada)
 				switch (stoi(principal[0])){
 
 				case 11:
-				cout<<principal.size();
-					std::cout << "11 " << (stoi(principal[1])) <<" " <<principal[2] <<" "<<principal[3] <<" "<<principal[4] <<" "<<stoi((principal[5])) <<" " <<principal[6] <<" "
-					<<principal[7]<<" " <<(stod(principal[8]))<<" " <<(stod(principal[9])) <<" "<<principal[10] <<" "<<principal[11]<<" "<<" "<<principal[12]<<std::endl;
+					cout<<principal.size()<<std::endl;
+
+					std::cout << "11 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<(stod(principal[8])) /*tam bico*/<<" " <<(stod(principal[9])) /*env. asas*/<<" "<<principal[10]/*aut. ibama*/ <<" "<<principal[11]/*uf origem*/<<" "<<principal[12]/*cidade origem*/ <<std::endl;
+					
 					/*AveNativo(11,stoi (principal[1]), principal[2], principal[3], principal[4], stoi(principal[5]), principal[6], 
-					principal[7], stod(principal[8]), stod(principal[9]),principal[10], principal[11] );*/
-					principal.clear();
+					principal[7], stod(principal[8]), stod(principal[9]),principal[10], principal[11], principal[12] );*/
 					
-				/*case "12":
-					AveExotico(12, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], 
-					principal[6], stod (principal[7]), stod(principal[8]), principal[9],principal[10] );
 					principal.clear();
-
-				case "21": 
-
-					AnfibioNativo(21, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
-					stoi(principal[7]), principal[8], principal[9], principal[10], principal[11]);
-					principal.clear();
+					break;
 					
-				case '22':
+				case 12:
+					cout<<principal.size()<<std::endl;
 				
+					std::cout << "11 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<(stod(principal[8])) /*tam bico*/<<" " <<(stod(principal[9])) /*env. asas*/<<" "<<principal[10]/*aut. ibama*/ <<" "<<principal[11]/*país origem*/ <<std::endl;
+					 /*
+					AveExotico(12, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], 
+					principal[6], stod (principal[7]), stod(principal[8]), principal[9],principal[10],principal[11] );
+					principal.clear();*/
+					
+					principal.clear();
+					break;
+					
+				case 21: 
+					cout<<principal.size()<<std::endl;
+
+					std::cout << "21 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<(stoi(principal[8])) /*total mudas*/<<" " <<principal[9] /*date ult. muda*/<<" "<<principal[10]/*aut. ibama*/ <<" "<<principal[11]/*uf origem*/<<" "<<principal[12]/*cidade origem*/ <<std::endl;
+					
+					/*
+					AnfibioNativo(21, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
+					stoi(principal[7]), principal[8], principal[9], principal[10], principal[11], principal[12]);
+					principal.clear();*/
+					principal.clear();
+					break;
+					
+				case 22:
+					cout<<principal.size()<<std::endl;
+					
+					std::cout << "22 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<(stoi(principal[8])) /*total mudas*/<<" " <<principal[9] /*date ult. muda*/<<" "<<principal[10]/*aut. ibama*/ <<" "<<principal[11]/*pais origem*/<<std::endl;
+										
+					/*
 					AnfibioExotico(22, principal[1], principal[2], principal[3],stoi(principal[4]), principal[5],principal[6],
-					stoi(principal[7]), principal[8], principal[9], principal[10]);
+					stoi(principal[7]), principal[8], principal[9], principal[10],principal[11]);
+					principal.clear();*/
 					principal.clear();
-				*/
+					break;
 				case 31:
-					cout<<principal.size();
-					std::cout << "31 " << stoi(principal[1]) <<" " <<principal[2] <<" "<<principal[3] <<" "<<principal[4] <<" "<<stoi(principal[5])<<" " <<principal[6] <<" "
-					<<principal[7]<<" " <<principal[8]<<" " <<stod(principal[9]) <<" "<<principal[10] <<" "<<" "<<principal[11] <<std::endl;
+					cout<<principal.size()<<std::endl;
+					
+					std::cout << "31 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<principal[8]/*cor pelo*/ <<" "<<principal[9]/*aut. ibama*/ <<" "<<principal[10]/*uf origem*/<<" "<<principal[11]/*cidade origem*/ <<std::endl;
+					
 					/*MamiferoNativo(31, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
-					principal[7], principal[8], principal[9], principal[10]);*/
+					principal[7], principal[8], principal[9], principal[10], principal[11]);*/
 					principal.clear();
+					break;
 					
 				case 32:
-					cout<<principal.size();
-					std::cout << "32 " << stoi(principal[1]) <<" " <<principal[2] <<" "<<principal[3] <<" "<<principal[4] <<" "<<stoi(principal[5])<<" " <<principal[6] <<" "
-					<<principal[7]<<" " <<principal[8]<<" " <<stod(principal[9]) <<" "<<principal[10] <<" " <<std::endl;
+					cout<<principal.size()<<std::endl;
 					
+					std::cout << "32 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<principal[8]/*cor pelo*/ <<" "<<principal[9]/*aut. ibama*/ <<" "<<principal[10]/*país origem*/<<std::endl;
+										
 					/*MamiferoExotico(31, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
-					principal[7], principal[8], principal[9]);*/
-					principal.clear();/*
-					
-				case "41":
-					ReptilNativo(41, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
-					bool m_venenoso(principal[7]), principal[8], principal[9], principal[10], principal[11]);
+					principal[7], principal[8], principal[9], principal[10]);*/
 					principal.clear();
+					break;
 					
-				case "42":
-					ReptilExotico(41, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
-					bool m_venenoso(principal[7]), principal[8], principal[9], principal[10]);
+				case 41:		
+					cout<<principal.size()<<std::endl;
+					
+					std::cout << "41 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<principal[8]/*venenoso?*/<<" " <<principal[9]/*tipo veneno*/ <<" "<<principal[10]/*aut. ibama*/ <<" "<<principal[11]/*uf origem*/<<" "<<principal[12]/*cidade origem*/ <<std::endl;
+										
+					/*ReptilNativo(41, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
+					bool m_venenoso(principal[7]), principal[8], principal[9], principal[10], principal[11], principal[12]);
 					principal.clear();*/
+					principal.clear();
+					break;
+					
+				case 42:
+					cout<<principal.size()<<std::endl;
+					std::cout << "42 "/*cod*/ << (stoi(principal[1]))/*id*/ <<" " <<principal[2] /*classe*/ <<" "<<principal[3] /*nome cientifico*/ <<" "<<principal[4] /*sexo*/<<" "<<stoi((principal[5])) /*tamanho*/<<" " <<principal[6]/*dieta*/ <<" "
+					<<principal[7]/*nome batismo*/<<" " <<principal[8]/*venenoso?*/<<" " <<principal[9]/*tipo veneno*/ <<" "<<principal[10]/*aut. ibama*/ <<" "<<principal[11]/*país origem*/<<" " <<std::endl;						
+					/*
+					ReptilExotico(41, principal[1], principal[2], principal[3], stoi(principal[4]), principal[5], principal[6],
+					bool m_venenoso(principal[7]), principal[8], principal[9], principal[10], principal[11]);
+					principal.clear();*/
+					principal.clear();
+					break;
 				}}
 			
 			
